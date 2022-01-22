200853604a5602be18367ffda147d1c1c9cc8044
MegalaSubramanian <98220722+MegalaSubramanian@users.noreply.github.com>
2022-01-22 23:42:37 +0530

    Add files via upload

---
+++ CarDetails.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class CarDetails {
+	
+	String ownerName = "Sumathi";
+	long ownerContactNum = 9587642157L;
+	String modelNumber = "2015 Honda CR-V EX-L";
+	short cubicCapacity = 1596;
+	int seatingCapacity = 740;
+	byte wheelBase = 5;
+	float kmUsed = 10254.65F;
+	char accidentHistory = 'N'; 
+	boolean dieselEngine = false;
+}
+
+
